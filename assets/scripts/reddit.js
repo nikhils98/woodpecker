7c9c45347c3e3a4d05f02804a6c019f340b33e2e
nikhil <nikhil.satiani@gmail.com>
2023-09-09 19:19:16 +0500

    icons and peck button

--- assets/scripts/reddit.js
+++ assets/scripts/reddit.js
@@ -7,6 +7,8 @@ window.addEventListener('load', function () {
 function addButton() {
   const post = document.querySelectorAll('[data-test-id="post-content"]')?.[0];
   const menuWrapper = post.childNodes[post.childNodes.length - 1]
-  const peckButton = htmlToElement('<button class="reddit-button">Peck</button>')
+  const peckButton = htmlToElement('<button class="reddit-button"><img id="icon-image" alt="acorn" width="20" height="20"/></button>')
   menuWrapper.appendChild(peckButton)
+  let icon = document.getElementById("icon-image");
+  icon.src = chrome.runtime.getURL("assets/images/acorn.png");
 }
